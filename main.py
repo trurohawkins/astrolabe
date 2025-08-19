36f77b77272a8a12be9460996e36cfa3b808296c
FoopyDoodoopants <trubhawkins@gmail.com>
2025-08-19 15:56:15 -0400

    curses in

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+from curses import *
+
+def main(stdscr):
+    stdscr.clear()
+    stdscr.addstr(0, 0, "Hello sexy Boi")
+    stdscr.refresh()
+    stdscr.getkey()
+
+if __name__ == "__main__":
+    wrapper(main)
