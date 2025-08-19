9e07308a793d726f3dce3485fa2cb703f2a1e950
FoopyDoodoopants <trubhawkins@gmail.com>
2025-08-19 19:04:03 -0400

    merged

--- config.py
+++ config.py
@@ -2,7 +2,4 @@ import fileman
 
 time = 0
 
-Mercury= {"name":"Mercury", "char":'M', "height":1, "width":2, "radius":2, "velocity":0.01}
-Earth = {"name":"Earth", "char":'E', "height":1, "width":2, "radius":5, "velocity":0.05}
-
 solar_system = fileman.load_solar_system('solar_system.json')
--- draw.py
+++ draw.py
@@ -1,6 +1,10 @@
 from astro import *
 import config
 
+def debug(message):
+    with open("debug.log", "a") as f:
+        f.write(f"{message}\n")
+
 def drawSolarSystem(stdscr):
     stdscr.clear()
     height, width = stdscr.getmaxyx()
@@ -8,9 +12,6 @@ def drawSolarSystem(stdscr):
     cx = width // 2
 
     stdscr.addstr(cy, cx, 'S')
-#    drawPlanet(config.Mercury, stdscr)
-#    drawPlanet(config.Earth, stdscr)
-
     for planet_name, planet_data in config.solar_system.items():
         drawPlanet(planet_data, stdscr)
     stdscr.refresh()
@@ -19,7 +20,9 @@ def drawPlanet(planet, stdscr):
     height, width = stdscr.getmaxyx()
     cy = height // 2
     cx = width // 2
-
-#    angle, distance, x, y = Planet_Position(config.time, planet["radius"], planet["width"], planet["height"], cx, cy, planet["velocity"], 0)
+    for i in range(360):
+        angle, distance, x, y = Planet_Position(i, planet["radius"], planet["width"], planet["height"], cx, cy, planet["angular_velocity"], 0)
+        stdscr.addstr(int(y), int(x), '.')#planet["char"])
     angle, distance, x, y = Planet_Position(config.time, planet["radius"], planet["width"], planet["height"], cx, cy, planet["angular_velocity"], 0)
     stdscr.addstr(int(y), int(x), planet["char"])
+
