4174af36330d285a59a01755735659ac31913e54
bobo-baggins <urbanecoguard@gmail.com>
2025-08-19 13:54:25 -0700

    Adding Astro.py

---
+++ astro.py
@@ -0,0 +1,16 @@
+import math
+
+
+def Planet_Position(time,radius height, width, origin_x, origin_y, angular_v, intial_phase):
+    #Calculate planetary anagle relative to Sun 
+    solar_angle = angular_v * time + intial_phase
+
+    #Calculate x,y coordiantes
+    planet_x = origin_x + radius * width  * math.cos(solar_angle)
+    planet_y = origin_y + radius * height * math.sin(solar_angle)
+
+    #Calculate distance from Sun
+    solar_distance = math.sqrt( ( planet_x**2 ) + ( planet_y**2) )
+
+    return (solar_angle, solar_distance, planet_x, planet_y)
+    
