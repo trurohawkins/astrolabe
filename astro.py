66e2a0ab2dedd848eba14605559b55b72fec77b8
bobo-baggins <urbanecoguard@gmail.com>
2025-08-19 14:03:36 -0700

    updated astro.py

--- astro.py
+++ astro.py
@@ -1,6 +1,15 @@
 import math
+"""
+time, seconds in simulation time
+radius, size of orbit
+width, deformatin of orbit along x-axies
+height, deformation of orbit along y-axies 
+(For a circle width = height = 1)
+angular_v , angular velocity in radians per seconds
+intial phase, radians that detirmines planets starting position typically zero
 
-Earth = {"name":"Earth", "char":'E', "height":1, "width":1, "radius":1, "velocity":0}
+"""
+Earth = {"name":"Earth", "char":'E', "height":1, "width":1, "radius":1, "velocity":10}
 
 def Planet_Position(time,radius, height, width, origin_x, origin_y, angular_v, intial_phase):
     #Calculate planetary anagle relative to Sun 
